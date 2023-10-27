557b11c7a3f4f1f3a179a94b9767db7f3f131f98
PaperKurane <omginlol@gmail.com>
2023-10-27 21:13:13 +0800

    Mandatory Save

--- 2Y_OOP_2324_ADeckOfCards/21Game.cs
+++ 2Y_OOP_2324_ADeckOfCards/21Game.cs
@@ -23,6 +23,7 @@ namespace _2Y_OOP_2324_ADeckOfCards
             DeckOfCards doc = new DeckOfCards(true);
             List<Card> playerHand = new List<Card>();
             List<Card> dealerHand = new List<Card>();
+            int choice = 0;
 
             //Card draw = doc.drawACard();
             //Console.WriteLine("Drawing a card");
@@ -32,51 +33,105 @@ namespace _2Y_OOP_2324_ADeckOfCards
             playerHand = doc.drawACard(2);
             dealerHand = doc.drawACard(2);
 
-            showHand(dealerHand);
-            playerTurn = !playerTurn;
-            showHand(playerHand);
-            playerTurn = !playerTurn;
-
-
             while (true)
             {
+                ShowHand(dealerHand);
+                playerTurn = !playerTurn;
+                ShowHand(playerHand);
+                playerTurn = !playerTurn;
+                Console.WriteLine("\n\n");
+
                 if (playerTurn)
                 {
+                    Console.Write("Will you Hit[1] or Stand[2]? ");
+                    choice = PlayerChoice();
+
+                    if (choice == 1)
+                    {
+
+                    }
+                    else if (choice == 2)
+                    {
+
+                    }
                 }
                 else
                 {
+                    //
                 }
 
+                Console.Clear();
                 Console.ReadKey();
             }
         }
 
-        public void showHand(List<Card> eitherHand)
+        public void ShowHand(List<Card> eitherHand)
         {
             List<Card> hand = eitherHand;
+            List<int> totalScore = new List<int>();
 
             if (playerTurn == false)
             {
-                Console.WriteLine("\nYour Hand: ");
+                Console.WriteLine("\n\nYour Hand: ");
 
                 foreach (Card c in hand)
                 {
                     Console.WriteLine($"{c.GetCardValue()} - The {c.GetCardFace()} of {c.GetCardSuit()}");
-                    handValues[1] = handValues[1] + c.GetCardValue();
+                    totalScore.Add(c.GetCardValue());
                 }
                 Console.WriteLine($"\nHand Value: {handValues[1]}");
             }
             else
             {
-                Console.WriteLine("\n\nDealer's Hand: ");
+                Console.WriteLine("\nDealer's Hand: ");
 
                 foreach (Card c in hand)
                 {
                     Console.WriteLine($"{c.GetCardValue()} - The {c.GetCardFace()} of {c.GetCardSuit()}");
-                    handValues[0] = handValues[0] + c.GetCardValue();
+                    totalScore.Add(c.GetCardValue());
                 }
                 Console.WriteLine($"\nHand Value: {handValues[0]}");
             }
+
+            ScoreTally(totalScore);
+        }
+
+        public int PlayerChoice()
+        {
+            while (true)
+            {
+                string uInput = "";
+                int iInput = 0;
+
+                uInput = Console.ReadLine();
+
+                try
+                {
+                    iInput = int.Parse(uInput);
+                    if (iInput == 1 || iInput == 2)
+                        return iInput;
+                    else
+                    {
+                        Console.WriteLine("Please pick a number between 1 or 2");
+                        Console.ReadKey();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("This program only currently accepts either 1 or 2 as inputs. Please try again!");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        public int ScoreTally(List<int> totalScore)
+        {
+            int finalScore = 0;
+
+            foreach(int score in totalScore)
+                finalScore += score;
+
+            return finalScore;
         }
     }
 }
